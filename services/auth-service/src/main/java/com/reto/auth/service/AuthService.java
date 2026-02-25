6229f5441df5857d1d97159236699cc2cb17a70a
ESTEBAN <juanlozanop021@gmail.com>
2026-02-25 09:44:29 -0500

    prueba

--- services/auth-service/src/main/java/com/reto/auth/service/AuthService.java
+++ services/auth-service/src/main/java/com/reto/auth/service/AuthService.java
@@ -4,7 +4,6 @@ import com.reto.auth.dto.LoginRequest;
 import com.reto.auth.dto.RegisterRequest;
 import com.reto.auth.entity.UserEntity;
 import com.reto.auth.repository.UserRepository;
-import com.reto.auth.security.JwtService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
