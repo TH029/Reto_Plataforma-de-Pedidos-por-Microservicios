7c8c42e65d784751007c36792eb689d99bcc1e20
Thais <ilianisp011@gmail.com>
2026-03-12 22:45:00 -0500

    Rabbit

---
+++ services/order-service/src/main/java/com/reto/order/config/producer/OrderProducer.java
@@ -0,0 +1,16 @@
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendOrder(String order) {
+        rabbitTemplate.convertAndSend("orders.queue", order);
+    }
+}
--- services/order-service/src/main/java/com/reto/order/controller/OrderController.java
+++ services/order-service/src/main/java/com/reto/order/controller/OrderController.java
@@ -1,4 +1,18 @@
-package com.reto.order.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class OrderController {
+
+    private final OrderProducer producer;
+
+    public OrderController(OrderProducer producer) {
+        this.producer = producer;
+    }
+
+    @GetMapping("/test-order")
+    public String testOrder() {
+        producer.sendOrder("Pedido creado desde order-service");
+        return "Mensaje enviado a RabbitMQ";
+    }
 }
